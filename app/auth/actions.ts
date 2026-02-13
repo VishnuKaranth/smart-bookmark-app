1743e49661c96bdd0ad1201718ad7da5ab1e55a8
Vishnu <vishnukaranth04@gmail.com>
2026-02-13 17:46:20 +0530

    Fixed broken middleware

--- app/auth/actions.ts
+++ app/auth/actions.ts
@@ -5,10 +5,23 @@ import { createClient } from '@/utils/supabase/server'
 
 export async function loginWithGoogle() {
     const supabase = await createClient()
+
+    const getURL = () => {
+        let url =
+            process.env.NEXT_PUBLIC_BASE_URL ?? // Set this to your site URL in production env.
+            process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+            'http://localhost:3000/'
+        // Make sure to include `https://` when not localhost.
+        url = url.includes('http') ? url : `https://${url}`
+        // Make sure to include a trailing `/`.
+        url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+        return url
+    }
+
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-            redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/auth/callback`,
+            redirectTo: `${getURL()}auth/callback`,
         },
     })
 
